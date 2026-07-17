9b239b973430e0f424de8186383fbc4cd9a45ab9
hirose <hiro010at@gmail.com>
2026-07-17 17:31:55 +0900

    ECSの情報を取得するためのModel

---
+++ Assets/Scripts/Features/SpawnStatus/SpawnStatusModel.cs
@@ -0,0 +1,33 @@
+using System;
+using R3;
+using Unity.Entities;
+using UnityDotsCrowdLab.Features.Movement;
+using VContainer.Unity;
+
+namespace UnityDotsCrowdLab.Features.SpawnStatus
+{
+    public interface ISpawnStatustModel
+    {
+        ReadOnlyReactiveProperty<int> Count { get; }
+    }
+    public class SpawnStatusModel : ISpawnStatustModel, ITickable, IDisposable
+    {
+        readonly ReactiveProperty<int> count = new(0);
+        public ReadOnlyReactiveProperty<int> Count => count;
+
+        EntityQuery query;
+
+        public SpawnStatusModel()
+        {
+            var world = World.DefaultGameObjectInjectionWorld;
+            query = world.EntityManager.CreateEntityQuery(typeof(MoveTarget));
+        }
+
+        public void Tick()
+        {
+            count.Value = query.CalculateEntityCount();
+        }
+
+        public void Dispose() => count.Dispose();
+    }
+}
