2aaff6cac3d5484afabdd07a08ad254408ddd47a
hirose <hiro010at@gmail.com>
2026-07-18 17:39:35 +0900

    インデント修正

--- Assets/Scripts/Features/Spawner/SpawnerData.cs
+++ Assets/Scripts/Features/Spawner/SpawnerData.cs
@@ -2,13 +2,13 @@ using Unity.Entities;
 
 namespace UnityDotsCrowdLab.Features.Spawner
 {
-  public struct SpawnerData : IComponentData
-  {
-    public Entity Prefab;
-    public int FactionID;
-    public Entity StartPoint;
-    public Entity TargetPoint;
-    public float Interval;
-    public float Timer;
-  }
+    public struct SpawnerData : IComponentData
+    {
+        public Entity Prefab;
+        public int FactionID;
+        public Entity StartPoint;
+        public Entity TargetPoint;
+        public float Interval;
+        public float Timer;
+    }
 }
