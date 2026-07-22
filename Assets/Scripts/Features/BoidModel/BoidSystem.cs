83a57d8ac67426b12148aa50321c864eb29b420b
hirose <hiro010at@gmail.com>
2026-07-22 09:33:10 +0900

    normalizesafeに変更＆回転処理前にboidForceの速度を再計算

--- Assets/Scripts/Features/BoidModel/BoidSystem.cs
+++ Assets/Scripts/Features/BoidModel/BoidSystem.cs
@@ -78,7 +78,7 @@ namespace UnityDotsCrowdLab.Features.BoidModel
                                 float3 separationVector = transform.ValueRO.Position - transformLookup[other].Position;
                                 float distance = math.length(separationVector);
                                 float reciprocalDistance = distance > 0f ? 1f / distance : 0f;
-                                separationForce += math.normalize(separationVector) * reciprocalDistance;
+                                separationForce += math.normalizesafe(separationVector) * reciprocalDistance;
 
                                 // 整列
                                 float3 otherVelocity = velocityLookup[other].Value;
@@ -159,6 +159,8 @@ namespace UnityDotsCrowdLab.Features.BoidModel
                 transform.ValueRW.Position += boidForce * state.WorldUnmanaged.Time.DeltaTime;
 
 
+                // 速度制限処理後の実速度で再計算
+                currentSpeed = math.length(boidForce);
                 // 進行方向に回転させる
                 if (currentSpeed > 0.0001f)
                 {
